9d3042cf200df8f226b2bfe90fa36cab57874f97
Alex Iepishev <oiepishev@playtika.com>
2021-09-29 01:31:29 +0300

    Add local storage to preserve title, genre, and collection name for population of details page

--- layout/scripts/insert_fragments.js
+++ layout/scripts/insert_fragments.js
@@ -1,5 +1,29 @@
 const BASE_PATH_TO_GALLERY = "../../../images/demo/gallery/"
 
+function setTitle(title) {
+    sessionStorage.setItem("title", title)
+}
+
+function setGenre(genre) {
+    sessionStorage.setItem("genre", genre)
+}
+
+function setCollectionName(collectionName) {
+    sessionStorage.setItem("collectionName", collectionName)
+}
+
+function getTitle() {
+    return sessionStorage.getItem("title");
+}
+
+function getGenre() {
+    return sessionStorage.getItem("genre");
+}
+
+function getCollectionName() {
+    return sessionStorage.getItem("collectionName");
+}
+
 function insert_header() {
     $(".wrapper.col1").load("/pages/header.html");
 }
@@ -9,6 +33,10 @@ function insert_footer() {
 }
 
 function insert_gallery_thumbnails(title, genre, collectionName) {
+    setTitle(title);
+    setGenre(genre);
+    setCollectionName(collectionName);
+
     $(".wrapper.col4").load("/pages/gallery/gallery_thumbnails.html",
         function () {
             $(".wrapper.col4 h2").text(title); // change h2 of thumbnails collection
@@ -42,13 +70,30 @@ function load_pretty_photo() {
 function populate_gallery_table_for(genre, collectionName) {
     $.getJSON(BASE_PATH_TO_GALLERY + genre + "/" + collectionName + "/" + collectionName + ".json")
         .done(function (data) {
-            $('#name').text(data.name);
+            $('#title').text(data.title);
             $('#author').text(data.author);
             $('#size').text(data.size);
             $('#materials').text(data.materials);
             $('#year').text(data.year);
         })
         .fail(function () {
-            console.error("No description was found for [" + collectionName + "]" );
+            console.error("No description was found for [" + collectionName + "]");
+        });
+}
+
+// access to details page is possible only from gallery page (with fragments)
+// so by this time the arguments should be saved in localStorage
+function populate_details_page(title, genre, collectionName) {
+    $.getJSON(BASE_PATH_TO_GALLERY + genre + "/" + collectionName + "/" + collectionName + ".json")
+        .done(function (data) {
+            $('#title').text(data.title);
+            $('#author').text("Author: " + data.author);
+            $('#size').text("Size (cm): " + data.size);
+            $('#materials').text("Materials: " + data.materials);
+            $('#year').text("Year: " + data.year);
+            $('#description').text("Description: " + data.description);
+        })
+        .fail(function () {
+            console.error("No description was found for [" + collectionName + "]");
         });
 }
