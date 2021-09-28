91049cec3ff21ab279136bd84942038775258601
Alex Iepishev <oiepishev@playtika.com>
2021-09-28 23:03:22 +0300

    Add description table and details link for pages with fragments

--- layout/scripts/insert_fragments.js
+++ layout/scripts/insert_fragments.js
@@ -8,25 +8,26 @@ function insert_footer() {
     $(".wrapper.col5").load("/pages/footer.html");
 }
 
-function insert_gallery_thumbnails(title, pathToImages) {
+function insert_gallery_thumbnails(title, genre, collectionName) {
     $(".wrapper.col4").load("/pages/gallery/gallery_thumbnails.html",
         function () {
             $(".wrapper.col4 h2").text(title); // change h2 of thumbnails collection
-            prepend_gallery_hrefs_with_path_to_images(pathToImages)
-            prepend_gallery_img_source_with_path_to_images(pathToImages)
-            load_pretty_photo()
+            populate_gallery_table_for(genre, collectionName);
+            prepend_gallery_hrefs_with_path_to_images(genre, collectionName);
+            prepend_gallery_img_source_with_path_to_images(genre, collectionName);
+            load_pretty_photo();
         });
 }
 
-function prepend_gallery_hrefs_with_path_to_images(pathToImages) {
-    $('.wrapper.col4 a').attr('href', function (i, href) { // prepend path for images
-        return BASE_PATH_TO_GALLERY + pathToImages + href;
+function prepend_gallery_hrefs_with_path_to_images(genre, collectionName) {
+    $('.wrapper.col4 li a').attr('href', function (i, href) { // prepend path for images
+        return BASE_PATH_TO_GALLERY + genre + "/" + collectionName + href;
     });
 }
 
-function prepend_gallery_img_source_with_path_to_images(pathToImages) {
-    $('.wrapper.col4 img').attr('src', function (i, src) { // prepend path for images
-        return BASE_PATH_TO_GALLERY + pathToImages + src;
+function prepend_gallery_img_source_with_path_to_images(genre, collectionName) {
+    $('.wrapper.col4 li a img').attr('src', function (i, src) { // prepend path for images
+        return BASE_PATH_TO_GALLERY + genre + "/" + collectionName + src;
     });
 }
 
@@ -36,4 +37,18 @@ function load_pretty_photo() {
         overlay_gallery: false,
         social_tools: false
     });
+}
+
+function populate_gallery_table_for(genre, collectionName) {
+    $.getJSON(BASE_PATH_TO_GALLERY + genre + "/" + collectionName + "/" + collectionName + ".json")
+        .done(function (data) {
+            $('#name').text(data.name);
+            $('#author').text(data.author);
+            $('#size').text(data.size);
+            $('#materials').text(data.materials);
+            $('#year').text(data.year);
+        })
+        .fail(function () {
+            console.error("No description was found for [" + collectionName + "]" );
+        });
 }
